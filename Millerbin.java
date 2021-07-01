38d9510fea8c482d5e74151c19a00ac63189b5be
VŨ XUÂN KHẢI <66871042+khaivu2601@users.noreply.github.com>
2021-07-01 10:01:53 +0700

    aaaaaaaaaaaaaaaaa

---
+++ Millerbin.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Millerbin extends BPL{
+    public static void Miller(int n,int t) {
+        int s=0;
+        int r=(n-1);
+        while (r%2==0){
+            s=s+1;
+            r=r/2;
+        }
+        for (int i = 0; i < t; i++) {
+            double randomDouble = Math.random();
+            randomDouble = randomDouble * n-2 + 2;
+            int a = (int) randomDouble;
+            System.out.println("Random number a : " + a);
+            int y = BPL(a, r, n);
+            if (y != 1 && y != n - 1) {
+                int j=1;
+                while (j<=s-1 && y!=n-1){
+                    y=BPL(y,2,n);
+                    if(y==1){
+                        System.out.println("hợp số");
+                        j=j+1;
+                    }
+                }
+                if(y!=n-1){
+                    System.out.println("hợp số");
+                }
+            }
+            else{
+                System.out.println("SNT");
+            }
+        }
+    }
+}
---
+++ Random_sreach.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Random_sreach extends Millerbin{
+    public static int SinhSo(){
+        double random= Math.random();
+        random=random * 1000 +3;
+        return (int) random;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("nhập t");
+        int t= sc.nextInt();
+        int n= SinhSo();
+        if(n%7==0){
+            System.out.println("again!!!");
+            n=SinhSo();
+            System.out.println("số sinh ngẫu nhiên là "+n);
+        }
+        System.out.println("số sinh ngẫu nhiên là "+n);
+        Miller(n,t);
+    }
+}
