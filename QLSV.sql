cb08a0a7128973c46a834c107dacc0eb6a532d6a
VŨ XUÂN KHẢI <66871042+khaivu2601@users.noreply.github.com>
2021-09-25 10:08:07 +0700

    Add files via upload

---
+++ QLSV.sql
@@ -0,0 +1,144 @@
+﻿CREATE DATABASE QLSV
+USE QLSV
+
+CREATE TABLE Sinhvien(
+
+MaSV VARCHAR(10) NOT NULL PRIMARY KEY, 
+hotenSV NVARCHAR(40), 
+gioitinh VARCHAR(3) , 
+ngaysinh DATE , 
+quequan NVARCHAR(40), 
+lop VARCHAR(10)
+
+)
+
+CREATE TABLE Monhoc(
+
+MaMH VARCHAR(10) NOT NULL PRIMARY KEY, 
+TenMH NVARCHAR(30), 
+DVHT INT
+
+)
+
+CREATE TABLE Ketqua(
+
+MaSV VARCHAR(10) NOT NULL , 
+maMH VARCHAR(10) NOT NULL , 
+Diem INT CHECK (Diem BETWEEN 0 AND 10) 
+
+) 
+ALTER TABLE dbo.Ketqua ADD  CONSTRAINT r1 FOREIGN KEY (MaSV) REFERENCES dbo.Sinhvien(MaSV)
+
+ALTER TABLE dbo.Ketqua ADD  CONSTRAINT r2 FOREIGN KEY (maMH) REFERENCES dbo.Monhoc(MaMH)
+INSERT INTO dbo.Sinhvien
+(
+    MaSV,
+    hotenSV,
+    gioitinh,
+    ngaysinh,
+    quequan,
+    lop
+)
+VALUES
+(   'at160527',        -- MaSV - varchar(10)
+    N'vũ xuân khải',       -- hotenSV - nvarchar(40)
+    'nam',        -- gioitinh - varchar(3)
+    '20010126', -- ngaysinh - datetime
+    N'Nam Định',       -- quequan - nvarchar(40)
+    'at16e'         -- lop - varchar(10)
+    ),
+	('ct2648',N'Vũ Công Thức','nam','20010223',N'Nam Định','ct4a'),
+	('at157283',N'Vũ văn a','nam','19990325',N'Nghệ an','ct5c'),
+	('dt62784',N'Vũ Huy Hoàn','nam','20070709',N'Hà nội','at16d'),
+	('ct5248',N'Vũ cao hoan ','nam','20030426',N'quảng nam ','ct4a'),
+	('at16782',N'nguyễn binh bong','nu','20020928',N'tphcm','dt4b'),
+	('dt16092',N'Đinh văn nơi','nam','19860206',N'quảng bình','at13a'),
+	('at140289',N'đặng thị ngọc thịnh','nu','20070604',N'quảng trị','ct5d'),
+	('ct030506',N'triệu thị huệ','nu','19980108',N'thái bình','at14e'),
+	('at160525',N'hoàng ngọc phong','nam','20030208',N'hải phòng','ct5c')
+INSERT INTO dbo.Monhoc
+(
+    MaMH,
+    TenMH,
+    DVHT
+)
+VALUES
+('159',N'mạng máy tính',2),
+('357',N'tin đại cương',2),
+(   '123',  -- MaMH - varchar(10)
+    N'lý thuyết csdl', -- TenMH - nvarchar(30)
+    3    -- DVHT - int
+    ),
+	('456',N'mật mã học',3),
+	('789',N'Thể chất cô hoa',15)
+INSERT INTO dbo.Ketqua
+(
+    MaSV,
+    maMH,
+    Diem
+)
+VALUES
+('at16782','357',3),
+('at16782','159',6),
+('dt16092','456',8),
+(   'at160527', -- MaSV - varchar(10)
+    '789', -- maMH - varchar(10)
+    7   -- Diem - int
+    ),
+	('at160525','456',6),
+	('at160527','456',8),
+	('at160525','789',3),
+	('ct030506','123',7),
+	('ct030506','789',3),
+	('at16782','123',5),
+	('at16782','456',6),
+	('at16782','789',5),
+	('ct2648','123',7),
+	('ct2648','456',6),
+	('ct2648','789',4)
+SELECT*FROM dbo.Ketqua
+SELECT*FROM dbo.Monhoc 
+SELECT*FROM dbo.Sinhvien
+
+--Cho biết mã môn học, tên môn học, điểm thi tất cả các môn của sinh viên tên Thức
+
+SELECT dbo.Monhoc.MaMH , dbo.Monhoc.TenMH , dbo.Ketqua.Diem 
+FROM dbo.Sinhvien,dbo.Monhoc,dbo.Ketqua
+WHERE dbo.Sinhvien.MaSV=dbo.Ketqua.MaSV AND dbo.Monhoc.MaMH=dbo.Ketqua.maMH AND hotenSV LIKE N'% thức'
+
+--Cho biết mã môn học, tên môn và điểm thi ở những môn mà sinh viên tên huệ phải thi lại
+--(điểm<5)
+SELECT dbo.Monhoc.MaMH , dbo.Monhoc.TenMH , dbo.Ketqua.Diem
+FROM dbo.Sinhvien , dbo.Monhoc , dbo.Ketqua
+WHERE dbo.Sinhvien.MaSV=dbo.Ketqua.MaSV AND dbo.Monhoc.MaMH=dbo.Ketqua.maMH AND hotenSV LIKE N'% huệ' AND Diem<=5
+
+--Cho biết mã sinh viên, tên những sinh viên đã thi ít nhất là 1 trong 3 môn Lý thuyết Cơ sở dữ
+--liệu, Tin học đại cương, mạng máy tính.
+select sv.Masv, sv.hotenSV, mh.TenMH
+from Sinhvien sv, Monhoc mh, Ketqua kq
+where sv.Masv = kq.Masv and mh.Mamh = kq.Mamh and Tenmh in (N'tin đại cương', N'lý thuyết csdl', N'Mạng máy tính')
+--Cho biết mã môn học, tên môn mà sinh viên có mã số 1 chưa có điểm
+SELECT dbo.Monhoc.MaMH,dbo.Monhoc.TenMH
+FROM dbo.Monhoc,dbo.Sinhvien,dbo.Ketqua
+EXCEPT
+SELECT dbo.Monhoc.MaMH,dbo.Monhoc.TenMH
+FROM dbo.Monhoc,dbo.Sinhvien,dbo.Ketqua
+WHERE dbo.Sinhvien.MaSV=dbo.Ketqua.MaSV AND dbo.Monhoc.MaMH=dbo.Ketqua.maMH AND Ketqua.MaSV='at160525' 
+
+--Cho biết điểm cao nhất môn 1 mà các sinh viên đạt được
+SELECT MAX(Diem)
+FROM dbo.Ketqua,dbo.Monhoc
+WHERE TenMH=N'thể chất cô hoa'
+--Cho biết mã sinh viên, tên những sinh viên có điểm thi môn 2 không thấp nhất khoa
+SELECT dbo.Sinhvien.hotenSV,dbo.Sinhvien.MaSV
+FROM dbo.Ketqua,dbo.Monhoc,dbo.Sinhvien
+WHERE dbo.Sinhvien.MaSV=dbo.Ketqua.MaSV AND dbo.Monhoc.MaMH=dbo.Ketqua.maMH AND TenMH=N'thể chất cô hoa' 
+AND Diem > (SELECT MIN(Diem) FROM dbo.Ketqua WHERE TenMH=N'thể chất cô hoa')
+--Cho biết mã sinh viên và tên những sinh viên có điểm thi môn 1 lớn hơn điểm thi môn 1 của
+--sinh viên có mã số 3
+
+
+
+
+
+
---
+++ thư viện.sql
@@ -0,0 +1,98 @@
+﻿CREATE DATABASE Thu_Vien
+-- tạo bảng 
+CREATE TABLE BANDOC(
+maBD INT  NOT NULL PRIMARY KEY, 
+hotenBD NVARCHAR(30) NOT NULL, 
+ngaySinh DATE CHECK (ngaySinh < GETDATE()), 
+lop CHAR(5), 
+quequan NVARCHAR(50), 
+sdt INT 
+) 
+
+CREATE TABLE SACH(
+maS VARCHAR(10) NOT NULL PRIMARY KEY, 
+TenS NVARCHAR(30) NOT NULL, 
+TheLoai VARCHAR(30), 
+Tacgia NVARCHAR(30), 
+NamXB INT, 
+NhaXB NVARCHAR(30)
+)
+CREATE TABLE PHIEUMUON(
+maBD INT  NOT NULL PRIMARY KEY, 
+maS VARCHAR(10) NOT NULL , 
+Ngaymuon DATETIME, 
+Ngayhentra DATETIME, 
+TraSach INT  
+)
+ALTER TABLE dbo.PHIEUMUON
+ADD CONSTRAINT r1 FOREIGN KEY (maBD) REFERENCES dbo.BANDOC
+ALTER TABLE dbo.PHIEUMUON
+ADD CONSTRAINT r2 FOREIGN KEY (maS) REFERENCES dbo.Sach
+
+INSERT INTO dbo.BANDOC
+(
+    maBD,
+    hotenBD,
+    ngaySinh,
+    lop,
+    quequan,
+    sdt
+)
+VALUES
+(   123,
+    N'vũ Xuân Khải',       -- hotenBD - nvarchar(30)
+    '20010126', -- ngaySinh - date
+    'at16e',        -- lop - char(5)
+    N'Nam Định',       -- quequan - nvarchar(50)
+    0869969623          -- sdt - int
+    ),
+	(124,N'Vũ Công Thức','20010223','at16c',N'Nam Định',1325465),
+	(125,N'Vũ văn a','19990325','at15b',N'Nghệ an',116462317),
+	(126,N'Vũ Huy Hoàn','20070709','at20b',N'Hà nội',3465464),
+	(127,N'Vũ cao hoan ','20030426','at19d',N'quảng nam ',3456465),
+	(128,N'nguyễn binh bong','20020928','at14g',N'tphcm',464251),
+	(129,N'Đinh văn nơi','19860206','at5d',N'quảng bình',4765415),
+	(130,N'đặng thị ngọc thịnh','20070604','ct4a',N'quảng trị',346546),
+	(131,N'triệu thị huệ','19980108','at15a',N'thái bình',3134684),
+	(132,N'hoàng ngọc phong','20030208','ct6d',N'hải phòng',87545132)
+INSERT INTO dbo.SACH
+(
+    maS,
+    TenS,
+    TheLoai,
+    Tacgia,
+    NamXB,
+    NhaXB
+)
+VALUES
+(   'c95b',  -- maS - varchar(10)
+    N'cơ sở dữ liệu', -- TenS - nvarchar(30)
+    'giáo trình',  -- TheLoai - varchar(30)
+    N'Đào Minh tấn', -- Tacgia - nvarchar(30)
+    2013,   -- NamXB - int
+    N'Giáo Dục'  -- NhaXB - nvarchar(30)
+    ),
+	('a531',N'Giải thuật lập trình','giáo trình',N'Nguyễn Văn Phác',2015,N'Quốc Gia'),
+	('c95a',N'tâm lý học','Tiểu thuyết',N'Đào tố Uyên',2009,N'Giáo Dục')
+INSERT INTO dbo.PHIEUMUON
+(	maBD,
+    maS,
+    Ngaymuon,
+    Ngayhentra,
+    TraSach
+)
+VALUES
+(   123,
+    'c95b',        -- maS - varchar(10)
+    '20210528', -- Ngaymuon - datetime
+    '20210603', -- Ngayhentra - datetime
+    1          -- TraSach - int
+    ),
+	(124,'c95a','20211106','20211111',1),
+	(129,'a531','20211012','20211015',1),
+	(130,'c95b','20210604','20210612',0),
+	(128,'a531','20201206','20210101',0)
+
+SELECT*FROM dbo.BANDOC
+SELECT*FROM dbo.SACH
+SELECT *FROM dbo.PHIEUMUON
